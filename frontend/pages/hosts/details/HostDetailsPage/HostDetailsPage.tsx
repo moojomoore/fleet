f9f00a55501cc5045eff894fe840860936f4a0bd
Rachel Perkins <rachel@Rachels-MacBook-Pro.local>
2026-04-21 15:37:46 -0400

    Headers don't use periods

--- frontend/pages/hosts/details/HostDetailsPage/HostDetailsPage.tsx
+++ frontend/pages/hosts/details/HostDetailsPage/HostDetailsPage.tsx
@@ -1235,7 +1235,7 @@ const HostDetailsPage = ({
                       />
                     </>
                   }
-                  header="Software library is currently not supported on this host."
+                  header="Software library is currently not supported on this host"
                 />
               ) : (
                 <SoftwareLibraryCard
